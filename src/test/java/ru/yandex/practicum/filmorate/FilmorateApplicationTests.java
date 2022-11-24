cf9b3080cac6fb3df33c1fdbdafd055d4392d9bd
NFilin <nickadm@yandex.ru>
2022-11-24 14:56:45 +0300

    fix: убрал дубль зависимостей

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,13 +1,33 @@
 package ru.yandex.practicum.filmorate;
 
+
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.user.UserDbStorage;
+
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 @SpringBootTest
-class FilmorateApplicationTests {
+@AutoConfigureTestDatabase
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+class FilmoRateApplicationTests {
+    private final UserDbStorage userStorage;
 
     @Test
-    void contextLoads() {
-    }
+    public void testFindUserById() {
 
-}
+        Optional<User> userOptional = userStorage.findUserById(1);
+
+        assertThat(userOptional)
+                .isPresent()
+                .hasValueSatisfying(user ->
+                        assertThat(user).hasFieldOrPropertyWithValue("id", 1)
+                );
+    }
+} }
